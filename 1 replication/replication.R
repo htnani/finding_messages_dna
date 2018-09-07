07ff09892ee2b92e3f6a3d895fd8745b022d5415
Jose A Dianes <jadianes@gmail.com>
2018-09-07 15:10:09 +0100

    frequent words

--- 1 replication/replication.R
+++ 1 replication/replication.R
@@ -1,4 +1,7 @@
-
+#' Counts the number of times pattern appears in text
+#' @param text
+#' @param pattern
+#' @return the number of times pattern shows in text
 patternCount <- function(text, pattern) {
   count <- 0
   pattern_length <- nchar(pattern)
@@ -11,9 +14,26 @@ patternCount <- function(text, pattern) {
   return(count)
 }
 
+#' 
+#' @param text
+#' @param k the length of the substring pattern to look for
+#' @return 
+frequentWords <- function(text, k) {
+  # Iterate through text counting for k-length substring patterns
+  count <- sapply(1:(nchar(text)-k), function(i) {
+    pattern <- substr(text, i, i+(k-1))
+    patternCount(text, pattern)
+  })
+  # Get most frequent pattern positions
+  max_count <- max(count)
+  max_count_i <- which(count==max_count)
+  # Substring using those positions
+  frequent_patterns <- sapply(max_count_i, function(i) {
+    substr(text, i, i+(k-1))
+  })
+  # remove duplicates
+  frequent_patterns <- unique(frequent_patterns)
+  
+  return(frequent_patterns)
+}
 
-input <- read.delim("dataset_2_7 (1).txt", header = F, stringsAsFactors = F)
-text <- input[1,1]
-pattern <- input[2,1]
-
-patternCount(text, pattern)
